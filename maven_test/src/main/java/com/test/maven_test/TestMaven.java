1f66e0459d113682666d2c25daebaacb267f939d
Jason <ck_developer2013@163.com>
2022-08-29 16:38:05 +0800

    Test fun

---
+++ maven_test/src/main/java/com/test/maven_test/TestMaven.java
@@ -0,0 +1,18 @@
+package com.test.maven_test;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * DES:
+ * <p>
+ * Date: 2022/8/29  16:36
+ *
+ * @author Jason
+ */
+public class TestMaven {
+
+    public static void showToast(Context context, String content) {
+        Toast.makeText(context, content, Toast.LENGTH_LONG).show();
+    }
+}
